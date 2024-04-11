81c9f8a3cb76354b9755a85cc8ad777c6df1bcce
Melany0802 <melcris0802@estudiantec.cr>
2024-04-11 16:39:16 -0600

    Category List Functionality

--- components/ListOfCategories.jsx
+++ components/ListOfCategories.jsx
@@ -1,16 +1,56 @@
-import React from 'react';
-import { Text, View, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, ScrollView, StyleSheet } from 'react-native';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
 const ListOfCategories = ({ navigation, showSidebar }) => {
-  return (
-    <View>
-              {showSidebar && <Sidebar navigation={navigation} />} 
+  const [categories, setCategories] = useState([]);
 
-      <Text>List of categories</Text>
+  useEffect(() => {
+    // Lógica para obtener la lista de categorías desde la API
+    fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
+      .then(response => response.json())
+      .then(data => {
+        if (data && data.meals) {
+          const categoriesData = data.meals.map(item => item.strCategory);
+          setCategories(categoriesData);
+        }
+      })
+      .catch(error => console.error('Error fetching categories:', error));
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {showSidebar && <Sidebar navigation={navigation} />}
+      <Text style={styles.title}>Categories FM</Text>
+      <ScrollView>
+        {categories.map(category => (
+          <View key={category} style={styles.categoryItem}>
+            <Text>{category}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  categoryItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCCCCC',
+  },
+});
 
 export default ListOfCategories;
